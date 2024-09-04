5e3e9c576c76e0783f4b833af195edaa7eed06cd
bdCalling <promotionbd321@gmail.com]>
2024-09-04 10:08:05 -0700

    icon completed

--- lib/utils/app_icons/app_icons.dart
+++ lib/utils/app_icons/app_icons.dart
@@ -25,6 +25,42 @@ class AppIcons {
   static const String mansion = "$basePath/mansion.svg";
   static const String villa = "$basePath/villa.svg";
 
+  ///================================Other====================
+  static const String about = "$basePath/about.svg";
+  static const String add = "$basePath/add.svg";
+  static const String addPhoto = "$basePath/add_photo.svg";
+  static const String addRoom = "$basePath/add_room.svg";
+  static const String allTask = "$basePath/all_task.svg";
+  static const String calendar = "$basePath/calendar.svg";
+  static const String completeTask = "$basePath/complete_task.svg";
+  static const String delete = "$basePath/delete.svg";
+  static const String details = "$basePath/details.svg";
+  static const String downUp = "$basePath/down_up.svg";
+  static const String download = "$basePath/download.svg";
+  static const String edit = "$basePath/edit.svg";
+  static const String favorite = "$basePath/favorite.svg";
+  static const String grocery = "$basePath/grocery.svg";
+  static const String help = "$basePath/help.svg";
+  static const String lock = "$basePath/lock.svg";
+  static const String logOut = "$basePath/logout.svg";
+  static const String myPlan = "$basePath/my_plan.svg";
+  static const String myRecipe = "$basePath/my_recipe.svg";
+  static const String notification = "$basePath/notification.svg";
+  static const String ongoing = "$basePath/ongoing.svg";
+  static const String pacages = "$basePath/pacages.svg";
+  static const String premium = "$basePath/premium.svg";
+  static const String privacy = "$basePath/privacy.svg";
+  static const String profileEdit = "$basePath/profile_edit.svg";
+  static const String rightError = "$basePath/right_error.svg";
+  static const String rightUp = "$basePath/right_up.svg";
+  static const String search = "$basePath/search.svg";
+  static const String setting = "$basePath/settings.svg";
+  static const String tags = "$basePath/tags.svg";
+  static const String terms = "$basePath/terms.svg";
+  static const String user = "$basePath/user.png";
+  static const String watch = "$basePath/watch.svg";
+  static const String x = "$basePath/x.svg";
+
 
 
 }
