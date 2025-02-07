9abaae891798c81948dfe7d11f5d08f33e604a7e
masum <promotionbd321@gmail.com>
2025-02-07 17:47:58 +0600

    sign in complete

--- lib/app/view/screens/home_owner_screen/authentication/forget_password_screen/forgot_password_screen.dart
+++ lib/app/view/screens/home_owner_screen/authentication/forget_password_screen/forgot_password_screen.dart
@@ -3,13 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:tidybayte/app/core/app_routes/app_routes.dart';
 import 'package:tidybayte/app/utils/app_colors/app_colors.dart';
-import 'package:tidybayte/app/utils/app_icons/app_icons.dart';
 import 'package:tidybayte/app/utils/app_images/app_images.dart';
 import 'package:tidybayte/app/utils/app_strings/app_strings.dart';
 import 'package:tidybayte/app/view/components/custom_button/custom_button.dart';
-import 'package:tidybayte/app/view/components/custom_image/custom_image.dart';
-import 'package:tidybayte/app/view/components/custom_task_details_dialoge/custom_task_details_dialoge.dart';
-import 'package:tidybayte/app/view/components/custom_text/custom_text.dart';
 import 'package:tidybayte/app/view/components/custom_text_field/custom_text_field.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
