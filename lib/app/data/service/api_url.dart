2d5a04f7b593636f4adcb99a4b30dff87629090e
masum <promotionbd321@gmail.com>
2025-02-07 16:17:12 +0600

    set up api

---
+++ lib/app/data/service/api_url.dart
@@ -0,0 +1,9 @@
+class ApiConstant {
+
+  static const baseUrl = "https://api.kidsknowrights.com";
+
+  ///<======================= For Auth part ====================>
+  static const register = "$baseUrl/user/register-user";
+
+
+}
