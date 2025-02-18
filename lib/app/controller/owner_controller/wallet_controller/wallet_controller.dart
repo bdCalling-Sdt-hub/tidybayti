231b2a0c7e2adf491e6e8957cd966f07b6d6038b
masum <promotionbd321@gmail.com>
2025-02-18 18:09:25 +0600

    budget get method create

--- lib/app/controller/owner_controller/wallet_controller/wallet_controller.dart
+++ lib/app/controller/owner_controller/wallet_controller/wallet_controller.dart
@@ -1,10 +1,58 @@
 import 'package:get/get.dart';
+import 'package:tidybayte/app/core/dependency/path.dart';
+import 'package:tidybayte/app/data/model/owner_model/budget_model.dart';
+import 'package:tidybayte/app/data/service/api_check.dart';
+import 'package:tidybayte/app/data/service/api_client.dart';
+import 'package:tidybayte/app/data/service/api_url.dart';
+import 'package:tidybayte/app/utils/app_const/app_const.dart';
 
 class WalletController extends GetxController{
+  ApiClient apiClient = serviceLocator();
 
+  void setRxRequestStatus(Status value) => rxRequestStatus.value = value;
+  final rxRequestStatus = Status.loading.obs;
+  //Budget Crate Loader
   var isLoading = false.obs;
 
   void setLoading(bool value) {
     isLoading.value = value;
   }
+
+  ///==================================✅✅Budget Get✅✅=======================
+
+  Rx<BudgetData> budgetData = BudgetData().obs;
+
+  getBudget() async {
+    setRxRequestStatus(Status.loading);
+    refresh();
+
+    try {
+      final response = await apiClient.get(url: ApiUrl.getMyBudget, showResult: true);
+
+      if (response.statusCode == 200) {
+        budgetData.value = BudgetData.fromJson(response.body["data"]);
+
+        print('StatusCode==================${response.statusCode}');
+        print(
+            'budgetData Result==================${budgetData.value.result?.length}');
+
+        setRxRequestStatus(Status.completed);
+        refresh();
+      } else {
+        setRxRequestStatus(Status.error);
+        ApiChecker.checkApi(response);
+      }
+    } catch (e) {
+      setRxRequestStatus(Status.error);
+      print('Error fetching data: $e');
+    }
+  }
+
+
+  @override
+  void onInit() {
+    getBudget();
+    super.onInit();
+  }
+
 }
