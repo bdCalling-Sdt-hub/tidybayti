dfc8d5aefdb0d321d22768436e04f000a052f0cf
masum <promotionbd321@gmail.com>
2025-02-18 18:17:17 +0600

    remove budget complete

--- lib/app/controller/owner_controller/wallet_controller/wallet_controller.dart
+++ lib/app/controller/owner_controller/wallet_controller/wallet_controller.dart
@@ -4,13 +4,15 @@ import 'package:tidybayte/app/data/model/owner_model/budget_model.dart';
 import 'package:tidybayte/app/data/service/api_check.dart';
 import 'package:tidybayte/app/data/service/api_client.dart';
 import 'package:tidybayte/app/data/service/api_url.dart';
+import 'package:tidybayte/app/utils/ToastMsg/toast_message.dart';
 import 'package:tidybayte/app/utils/app_const/app_const.dart';
 
-class WalletController extends GetxController{
+class WalletController extends GetxController {
   ApiClient apiClient = serviceLocator();
 
   void setRxRequestStatus(Status value) => rxRequestStatus.value = value;
   final rxRequestStatus = Status.loading.obs;
+
   //Budget Crate Loader
   var isLoading = false.obs;
 
@@ -27,7 +29,8 @@ class WalletController extends GetxController{
     refresh();
 
     try {
-      final response = await apiClient.get(url: ApiUrl.getMyBudget, showResult: true);
+      final response =
+          await apiClient.get(url: ApiUrl.getMyBudget, showResult: true);
 
       if (response.statusCode == 200) {
         budgetData.value = BudgetData.fromJson(response.body["data"]);
@@ -48,11 +51,59 @@ class WalletController extends GetxController{
     }
   }
 
+  ///==================================✅✅Budget Single Get✅✅=======================
+
+  // Rx<BudgetData> budgetData = BudgetData().obs;
+
+  getSingleBudget({required String budgetId}) async {
+    setRxRequestStatus(Status.loading);
+    refresh();
+
+    try {
+      final response = await apiClient.get(
+          url: ApiUrl.getSingleBudget(budgetId), showResult: true);
+
+      if (response.statusCode == 200) {
+        // budgetData.value = BudgetData.fromJson(response.body["data"]);
+
+        print('StatusCode==================${response.statusCode}');
+        print(
+            'budgetData Result==================${budgetData.value.result?.length}');
+
+        setRxRequestStatus(Status.completed);
+        refresh();
+      } else {
+        setRxRequestStatus(Status.error);
+        ApiChecker.checkApi(response);
+      }
+    } catch (e) {
+      setRxRequestStatus(Status.error);
+      print('Error fetching data: $e');
+    }
+  }
+
+  ///==================================✅✅Remove Budget✅✅=======================
+  RxBool isRemoveBudget = false.obs;
+
+  removeTask({required String budgetId}) async {
+    isRemoveBudget.value = true;
+    var body = {"budgetId": budgetId};
+
+    var response = await apiClient.delete(body: body, url: ApiUrl.deleteBudget);
+    if (response.statusCode == 200) {
+      toastMessage(message: response.body["message"]);
+    } else if (response.statusCode == 400) {
+      toastMessage(message: response.body["message"]);
+    } else {
+      ApiChecker.checkApi(response);
+    }
+    isRemoveBudget.value = false;
+    isRemoveBudget.refresh();
+  }
 
   @override
   void onInit() {
     getBudget();
     super.onInit();
   }
-
 }
