b3e975eab1c04955f6be8ee29c62f22f8afb231a
masum <promotionbd321@gmail.com>
2025-02-24 17:11:19 +0600

    my recipe complete

--- lib/app/view/screens/home_owner_screen/recipe_screen/favorite_recipes_screen/favorites_recipe_screen.dart
+++ lib/app/view/screens/home_owner_screen/recipe_screen/favorite_recipes_screen/favorites_recipe_screen.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:tidybayte/app/core/app_routes/app_routes.dart';
-import 'package:tidybayte/app/utils/app_colors/app_colors.dart';
 import 'package:tidybayte/app/utils/app_const/app_const.dart';
-import 'package:tidybayte/app/utils/app_icons/app_icons.dart';
 import 'package:tidybayte/app/utils/app_images/app_images.dart';
 import 'package:tidybayte/app/utils/app_strings/app_strings.dart';
-import 'package:tidybayte/app/view/components/custom_button/custom_button.dart';
-import 'package:tidybayte/app/view/components/custom_image/custom_image.dart';
+
 import 'package:tidybayte/app/view/components/custom_menu_appbar/custom_menu_appbar.dart';
 import 'package:tidybayte/app/view/components/custom_recipe_card/custom_recipe_card.dart';
-import 'package:tidybayte/app/view/components/custom_task_details_dialoge/custom_task_details_dialoge.dart';
-import 'package:tidybayte/app/view/components/custom_text/custom_text.dart';
-import 'package:tidybayte/app/view/components/custom_text_field/custom_text_field.dart';
+
 import 'package:tidybayte/app/view/components/nav_bar/nav_bar.dart';
 
 class FavoritesRecipeScreen extends StatelessWidget {
@@ -40,7 +35,7 @@ class FavoritesRecipeScreen extends StatelessWidget {
                 children: [
                   /// Menu Title
                   CustomMenuAppbar(
-                    title: AppStrings.favoriteRecipes,
+                    title: AppStrings.favoriteRecipes.tr,
                     onBack: () {
                       Get.back();
                     },
